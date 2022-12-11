49ac567074fcab1ef9d25d59732f6c527bdb277d
Basil ALBADER <baalbade@student.42.fr>
2022-12-11 14:48:49 +0100

    add print str function

--- src/characters/ft_print_char.c
+++ src/characters/ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 10:44:39 by baalbade          #+#    #+#             */
-/*   Updated: 2022/12/01 12:05:03 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/12/11 14:48:15 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,15 @@ void	ft_print_char(char c, int *tot_len)
 {
 	write(1, &c, 1);
 	(*tot_len)++;
+}
+
+void	ft_print_str(char *str, int *tot_len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	write(1, str, i);
+	tot_len += i;
 }
