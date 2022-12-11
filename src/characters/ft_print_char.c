5b426d213dc3e05e729efb6077cb3d45e2c05ed4
Basil ALBADER <baalbade@student.42.fr>
2022-12-11 15:12:16 +0100

    ft_print_char.c

--- src/characters/ft_print_char.c
+++ src/characters/ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 10:44:39 by baalbade          #+#    #+#             */
-/*   Updated: 2022/12/11 14:50:28 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/12/11 15:04:58 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,4 @@ void	ft_print_char(char c, int *tot_len)
 {
 	write(1, &c, 1);
 	(*tot_len)++;
-}
-
-void	ft_print_str(char *str, int *tot_len)
-{
-	size_t	i;
-
-	if (!str)
-	{
-		(*tot_len) = 0;
-		return ;
-	}
-	i = 0;
-	while (str[i])
-	{
-		ft_print_char(str[i], tot_len);
-		++i;
-	}
 }
