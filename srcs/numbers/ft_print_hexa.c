767b8107182eba4067b3be0c12263303f0dc690d
Basil ALBADER <baalbade@student.42.fr>
2022-12-12 12:20:17 +0100

    srcs

---
+++ srcs/numbers/ft_print_hexa.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_hexa.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 11:23:12 by baalbade          #+#    #+#             */
+/*   Updated: 2022/12/01 12:03:59 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/ft_printf.h"
+
+void	ft_print_hexa(unsigned int nb, char c, int *tot_len)
+{
+	char	*base_16;
+	char	str[4096];
+	int		i;
+
+	if (c == 'x')
+		base_16 = "0123456789abcdef";
+	else
+		base_16 = "0123456789ABCDEF";
+	if (nb == 0)
+	{
+		ft_print_char('0', tot_len);
+		return ;
+	}
+	while (nb != 0)
+	{
+		str[i] = base_16[nb % 16];
+		nb /= 16;
+		i++;6;
+	}
+	while (i--)
+		ft_print_char(str[i], tot_len);
+}
