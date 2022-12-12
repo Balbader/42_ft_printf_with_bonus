f0ab19b9ba26a10f4f800e03d3c14ce9fe040ec5
Basil ALBADER <baalbade@student.42.fr>
2022-12-12 15:11:14 +0100

    ft_print_ptr.c

--- srcs/numbers/ft_print_ptr.c
+++ srcs/numbers/ft_print_ptr.c
@@ -6,13 +6,32 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 12:02:13 by baalbade          #+#    #+#             */
-/*   Updated: 2022/12/01 12:03:32 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/12/12 15:10:39 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/ft_printf.h"
+#include "../../includes/ft_printf.h"
 
 void	ft_print_ptr(size_t ptr, int *tot_len)
 {
+	char	*base_16;
+	char	ptr_str[4096];
+	int		i;
 
+	base_16 = "0123456789abcdef";
+	i = 0;
+	if (ptr == 0)
+	{
+		ft_print_str("(nil)", tot_len);
+		return ;
+	}
+	while (ptr != 0)
+	{
+		ptr_str[i] = base_16[ptr % 16];
+		ptr /= 16;
+		++i;
+	}
+	ptr_str[i] = '\0';
+	while (i--)
+		ft_print_char(ptr_str[i], tot_len);
 }
