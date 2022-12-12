a68e08041d4ce1bf712caf8a58bcff5c503f7892
Basil ALBADER <baalbade@student.42.fr>
2022-12-12 15:12:53 +0100

    ft_print_str.c

--- srcs/characters/ft_print_str.c
+++ srcs/characters/ft_print_str.c
@@ -6,11 +6,11 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 10:47:24 by baalbade          #+#    #+#             */
-/*   Updated: 2022/12/11 15:10:45 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/12/12 15:00:23 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/ft_printf.h"
+#include "../../includes/ft_printf.h"
 
 void	ft_print_str(char *str, int *tot_len)
 {
