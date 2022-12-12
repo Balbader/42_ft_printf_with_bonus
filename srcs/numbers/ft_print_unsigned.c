b2d684d25a96ca8a237b9822a3e6d78137f62170
Basil ALBADER <baalbade@student.42.fr>
2022-12-12 15:19:05 +0100

    ft_print_unsigned.c

---
+++ srcs/numbers/ft_print_unsigned.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_unsigned.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 15:13:35 by baalbade          #+#    #+#             */
+/*   Updated: 2022/12/12 15:18:51 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/ft_printf.h"
+
+void	ft_print_unsigned(unsigned int u, int *tot_len)
+{
+	if (u >= 10)
+		ft_print_hexa(u / 10, tot_len);
+	ft_print_char(u % 10 + '0', tot_len);
+}
